8d28a94a9790c16e789db69d51b8f0fd0361e1ee
Utkarsh Mehta <utkarshmehta143@gmail.com>
2022-04-04 12:36:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+// import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'uni-cracy';
+export class AppComponent implements OnInit {
+  policyForm!: FormGroup;
+  // faInfoCircle = faInfoCircle;
+
+  ngOnInit(): void {
+    this.policyForm = new FormGroup({
+      name: new FormControl(''),
+      gender: new FormControl('f'),
+      dob: new FormControl('', [Validators.required]),
+      tags: new FormControl('', [Validators.required]),
+      policy: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get name() {
+    return this.policyForm.get('name');
+  }
+  get gender() {
+    return this.policyForm.get('gender');
+  }
+  get dob() {
+    return this.policyForm.get('dob');
+  }
+  get tags() {
+    return this.policyForm.get('tags');
+  }
+  get policy() {
+    return this.policyForm.get('policy');
+  }
+
+  onSubmit() {
+    console.warn(this.policyForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
